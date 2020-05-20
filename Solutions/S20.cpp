450e5d95d0459280945e26d06e86fea082dca045
Bharat123rox <bharatraghunthan9767@gmail.com>
2020-05-20 16:18:34 +0530

    Add definition of tree node for clarity

--- Solutions/S20.cpp
+++ Solutions/S20.cpp
@@ -1,3 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 //Pre-Order traversal of a BST gives elements in sorted order
 vector<int> preOrder(TreeNode* root, vector<int> &v) {
     if(root) {
